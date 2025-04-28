b6149d67f6541e2a4b1b91a3ce06614931713d73
Juunibyou <danijunn@gmail.com>
2025-04-27 23:18:04 -0500

    update

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -3,4 +3,58 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 
+
+class DishTest{
+
+    private DishStack stack;
+
+    @BeforeEach
+    void set(){
+        stack = new DishStack();
+    }
+
+        //REGULAR TEST
+        @Test
+        void testPush(){
+            Dish oneDish = new Dish("A dish with one fish pattern on it");
+            Dish twoDish = new Dish("A dish with two fish patterns on it");
+
+            stack.push(oneDish);
+            stack.push(twoDish);
+
+            assertEquals(2, stack.size());
+        }
+
+        @Test
+        void testPop(){
+            Dish oneDish = new Dish("A dish with one fish pattern on it");
+            Dish twoDish = new Dish("A dish with two fish patterns on it");
+            stack.push(oneDish);
+            stack.push(twoDish);
+
+            stack.pop();
+            assertEquals(1, stack.size());
+        }
+
+        @Test
+        void testPeek(){
+            Dish oneDish = new Dish("A dish with one fish pattern on it");
+            Dish twoDish = new Dish("A dish with two fish patterns on it");
+
+            stack.push(oneDish);
+            stack.push(twoDish);
+
+            Dish peekdish = stack.peek();
+
+            assertEquals("A dish with two fish pattern on it", peekdish.getDescription());
+        }
+
+        //EMPTY OR FULL TEST
+        @Test
+        void emptyPop(){
+            assertEquals("Stack is empty... Can't pop.", stack.pop());
+        }
+
+}
